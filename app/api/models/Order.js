db985e0512985db02a6e1c8fc434b2a583195926
SamratAdhikari <samratmetaladhikari@gmail.com>
2024-12-13 12:15:44 +0545

    add Order Schema

---
+++ app/api/models/Order.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+// Define the schema for an order
+const orderSchema = new mongoose.Schema({
+    orderedTime: {
+        type: Date,
+        required: true,
+        default: Date.now,
+    },
+    tableNumber: {
+        type: Number,
+        required: true,
+    },
+    items: [
+        {
+            name: {
+                type: String,
+                required: true,
+            },
+        },
+    ],
+    status: {
+        type: String,
+        enum: ["order placed", "cooking", "completed"],
+        required: true,
+        default: "order placed",
+    },
+});
+
+// Export the model
+const Order = mongoose.model("User", userSchema);
+
+export default Order;
