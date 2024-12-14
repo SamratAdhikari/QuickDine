b40ee21439541e43005424fea0311c194eadc67d
SamratAdhikari <samratmetaladhikari@gmail.com>
2024-12-14 13:45:12 +0545

    fixed deployment

---
+++ app/(routes)/menu/_components/ChatBot.jsx
@@ -0,0 +1,19 @@
+// import Chatbot from "react-chatbot-deepai";
+// import "react-chatbot-deepai/dist/style.css";
+
+// function Chat() {
+//     return (
+//         <div>
+//             <Chatbot
+//                 OPEN_AI_KEY="SOME_KEY"
+//                 model="gpt-4"
+//                 max_tokens={150}
+//                 chatbot_prompt="You are an assistant for a clothing company called Love Clothing."
+//                 placeholderText="Type a message..."
+//                 buttonText="Chat with us"
+//             />
+//         </div>
+//     );
+// }
+
+// export default Chat;
